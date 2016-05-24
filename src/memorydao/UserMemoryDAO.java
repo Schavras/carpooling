0f0e84c4044f49f8d634900e4f087e5f70fdc46c
Team1 <Team1@665182bd-5f02-4a75-a8ef-b02fbe58bb79>
2016-05-24 06:01:22 +0000

    TODO diorthoseis nikou

--- src/memorydao/UserMemoryDAO.java
+++ src/memorydao/UserMemoryDAO.java
@@ -8,13 +8,13 @@ import dao.UserDAO;
 import domain.Place;
 import domain.Trip;
 import domain.User;
-import types.Email;
-import types.Password;
 
 public class UserMemoryDAO implements UserDAO  {
 	
-	protected static ArrayList<User> users = new ArrayList<User>();
-	protected static ArrayList<Trip> trips = new ArrayList<Trip>(); 
+	protected static List<User> users = new ArrayList<User>();
+	
+	//TODO se alli klasi
+	protected static List<Trip> trips = new ArrayList<Trip>(); 
 	
 	@Override
 	public User getByEmail(String email) {
@@ -22,22 +22,25 @@ public class UserMemoryDAO implements UserDAO  {
 		// User ni=new User();
 		 int k=-1;
 		// users.add(ni);
+		 //TODO kanto na girnaei ton user me to pou ton vrei, min psaxnei olo ton pinaka, kathe user einai mia fora mono
 		 for (int i = 0; i<users.size(); i++){
 			 k=users.indexOf(email);
 		 }
 		 if(k == -1){
-			 System.out.println("User not found");
+			// System.out.println("User not found");
 			 return null;
 		 }else{
 			 return users.get(k);
 		 }
 	}
 
+	//TODO Min kaneis elexno ama iparxei idi, apla na to prostheteis
 	@Override
 	public boolean newUser(User new_user) {
 		
 		int k=1;
 		for (int i = 0; i<users.size(); i++){
+			// TODO equals anti gia ==
 			 if(users.get(i).getEmail()==new_user.getEmail()){
 				 k=-1;
 			 }
@@ -50,12 +53,15 @@ public class UserMemoryDAO implements UserDAO  {
 		}
 	}
 	
-	public ArrayList<Trip> find(String country, String region, String town){
+	
+	//TODO valto se alli ksexoristi klasi mazi me to list trips, kai alakse to onoma se findByPlace
+	public List<Trip> find(String country, String region, String town){
 		
 		Place the_place=new Place(country,region,town);
 		ArrayList<Trip> the_trips = new ArrayList<Trip>(); 
 		
 		for (int i = 0; i<users.size(); i++){
+			//TODO equals anti gia ==
 			if(trips.get(i).getDestination()==the_place){
 				the_trips.add(trips.get(i));
 			}
