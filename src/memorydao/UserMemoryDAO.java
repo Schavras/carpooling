c06c9331332c27519812ddf1ae5fa0463c62b9d7
Team1 <Team1@665182bd-5f02-4a75-a8ef-b02fbe58bb79>
2016-05-24 05:42:54 +0000

    UserMemoryDAO finished

--- src/memorydao/UserMemoryDAO.java
+++ src/memorydao/UserMemoryDAO.java
@@ -1,20 +1,67 @@
 package memorydao;
 
+import java.util.ArrayList;
+import java.util.List;
+
+//import com.mgiandia.library.domain.Borrower;
+
 import dao.UserDAO;
+import domain.Place;
+import domain.Trip;
 import domain.User;
+import types.Email;
+import types.Password;
 
 public class UserMemoryDAO implements UserDAO  {
-
+	
+	protected static ArrayList<User> users = new ArrayList<User>();
+	protected static ArrayList<Trip> trips = new ArrayList<Trip>(); 
+	
 	@Override
 	public User getByEmail(String email) {
-		// TODO NIKO FTIAKSTO
-		return null;
+		
+		// User ni=new User();
+		 int k=-1;
+		// users.add(ni);
+		 for (int i = 0; i<users.size(); i++){
+			 k=users.indexOf(email);
+		 }
+		 if(k == -1){
+			 System.out.println("User not found");
+			 return null;
+		 }else{
+			 return users.get(k);
+		 }
 	}
 
 	@Override
 	public boolean newUser(User new_user) {
-		// TODO Auto-generated method
-		return false;
+		
+		int k=1;
+		for (int i = 0; i<users.size(); i++){
+			 if(users.get(i).getEmail()==new_user.getEmail()){
+				 k=-1;
+			 }
+		 }
+		if (k==-1){
+			return false;
+		}else{
+		users.add(new_user);
+		return true;
+		}
+	}
+	
+	public ArrayList<Trip> find(String country, String region, String town){
+		
+		Place the_place=new Place(country,region,town);
+		ArrayList<Trip> the_trips = new ArrayList<Trip>(); 
+		
+		for (int i = 0; i<users.size(); i++){
+			if(trips.get(i).getDestination()==the_place){
+				the_trips.add(trips.get(i));
+			}
+		 }
+		return the_trips;
 	}
 
 }
